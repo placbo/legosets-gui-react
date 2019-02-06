a4266bf8e76914daa5b7772cd8b4db2d3ef09a3f
placbo <Avulvab13>
2019-02-06 13:23:23 +0100

    rebrickable links styling

--- src/components/Set.js
+++ src/components/Set.js
@@ -5,11 +5,13 @@ function Set(props) {
     // const hasRebrickableData = "setDataFromRebrickable" in props.setData;
     return (
         <div className="set">
-            {"setDataFromRebrickable" in props.setData? (
+            {"setDataFromRebrickable" in props.setData ? (
+                <a target="_blank" href={props.setData.setDataFromRebrickable.set_url}>
                     <img alt="legoimage" src={props.setData.setDataFromRebrickable.set_img_url}/>
-                ) : (
-                    <img alt="legoimage" src="https://via.placeholder.com/100"/>
-                )}
+                </a>
+            ) : (
+                <img alt="legoimage" src="https://via.placeholder.com/100"/>
+            )}
             <div>{props.setData.setid}</div>
         </div>
     );
