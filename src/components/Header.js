a935bfffeb84f9f66072b071b0222c3e01f35a1a
pcb <per.christian.bjelke@bibsys.no>
2019-02-26 10:05:32 +0100

    added styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 
 function Header(props) {
+
     return (
         <header>
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
+                    <li className="rightMenuItem"><Link to='/login'>Login</Link></li>
                 </ul>
             </nav>
         </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
+import './Main.css';
 
 function Main(props) {
     return (
-        <main>
+        <div className="main">
             <Switch>
                 <Route path='/login' component={Login}/>
                 <Route exact path='/' component={Home}/>
             </Switch>
-        </main>
+        </div>
     );
 }
 
